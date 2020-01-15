a697c208c98ed8d3d47d38e6d9328867df88ebae
jesteban <jesteban@mit.edu>
2020-01-15 14:42:51 -0500

    connected backend routing with frontend axios requests

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
+  
 module.exports = {
-  transpileDependencies: ["vuetify"]
-};
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+      },
+
+      '/users': {
+        target: 'http://localhost:3000',
+      }
+    }
+  }
+}
