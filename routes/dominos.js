658c2a19f78467553f5bc0d292dc68200e9d81ee
jesteban <jesteban@mit.edu>
2020-01-25 00:51:42 -0500

    improved checkout ui

--- routes/dominos.js
+++ routes/dominos.js
@@ -3,27 +3,70 @@ const storeID = 3748; //1033 Mass. Ave
 
 var pizzapi = require('dominos');
 var myStore = new pizzapi.Store(storeID);
-myStore.ID = storeID;
+myStore.ID = 3748;
+
+var order = new pizzapi.Order(
+  {
+    "customer":{
+      "firstName":"Jonathan",
+      "lastName":"Esteban",
+      "address":{
+        "Street": "229 Vassar Street",
+        "City": "Cambridge",
+        "Region": "MA",
+        "PostalCode": "02139"
+      },
+      "email":"jesteban@mit.edu",
+      "phone":"7875480992"
+    },
+    "storeID":3748,
+    "deliveryMethod":"Delivery"
+  }
+);
+
+order.StoreID = 3748;
+order.StoreOrderID = order.storeID;
 
 const router = express.Router();
 const menu = require('../menu.json');
-
+const bigMenu = require('../bigMenu.json');
 
 /* GET /api/dominos page. */
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   myStore.getFriendlyNames(
-    function(storeData){
+    function (storeData) {
       res.send(storeData);
     }
-);
+  );
 });
 /* GET /api/dominos/menu page. */
-router.get('/menu', function(req, res) {
+router.get('/menu', function (req, res) {
   myStore.getFriendlyNames(
-    function(storeData){
+    function (storeData) {
       res.json(menu);
     }
-);
+  );
+});
+
+/* GET /api/dominos/pricing page. */
+router.get('/pricing/:product', function (req, res) {
+  let productCode = req.params.product;
+  let variantCode = bigMenu.result.Products[productCode].Variants[0];
+
+  order.addItem(
+    new pizzapi.Item(
+      {
+        code: variantCode,
+        options: [],
+        quantity: 1
+      }
+    )
+  );
+  order.price(
+    function (response) {
+      res.send("$" + response.result.Order.Amounts.Payment);
+    }
+  );
 });
 
 module.exports = router;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,55 +1,32 @@
 <template>
   <div class="home">
-    <v-container>
+    <v-container fluid>
       <v-row>
-        <v-col cols="10">
+        <v-col cols="9">
           <template>
             <v-expansion-panels focusable>
-              <v-expansion-panel
-                v-for="(items, category) in menu"
-                v-bind:key="category.id"
-              >
-                <v-expansion-panel-header v-text="category"/>
+              <v-expansion-panel v-for="(items, category) in menu" v-bind:key="category.id">
+                <v-expansion-panel-header v-text="category" />
                 <v-expansion-panel-content>
-
                   <v-container fluid>
-                    <v-row
-                      
-                      justify="start"
-                      align="center"
-                      
-                    >
-                      <v-col
-                        v-for="item in items"
-                        v-bind:key="item.id"
-                        cols="auto"
-                      >
-                        <v-card
-                          hover
-                          max-width="300px"
-                        >
+                    <v-row justify="start" align="center">
+                      <v-col v-for="item in items" v-bind:key="item.id" cols="auto">
+                        <v-card hover max-width="300px">
                           <v-img
-                            src="../assets/pizza.png"
+                            v-bind:src="item.imageURL"
                             lazy-src="../assets/pizza-min.png"
                             class="black--text align-end"
-                            gradient="to bottom, rgba(0,0,0,0), rgba(255,255,255,1)"
+                            gradient="to bottom, rgba(0,0,0,0) 80%, white 100%"
                             aspect-ratio
                           >
                             <template v-slot:placeholder>
-                              <v-row
-                                class="fill-height ma-0"
-                                align="center"
-                                justify="center"
-                              >
+                              <v-row class="fill-height ma-0" align="center" justify="center">
                                 <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
                               </v-row>
                             </template>
                           </v-img>
-                          <v-card-title 
-                          v-text="item.name"
-                          style="word-break: normal"
-                          >
-                          </v-card-title>
+                          <v-card-title v-text="item.name" style="word-break: normal"></v-card-title>
+                          <v-card-subtitle>$5</v-card-subtitle>
                           <v-card-actions>
                             <v-spacer></v-spacer>
 
@@ -66,27 +43,36 @@
                       </v-col>
                     </v-row>
                   </v-container>
-
                 </v-expansion-panel-content>
               </v-expansion-panel>
             </v-expansion-panels>
           </template>
         </v-col>
         <v-col>
-          <v-card>
-            {{ addedToCart }}
-            <v-card-actions>
-                            <v-spacer></v-spacer>
-
-                            <v-btn
-                              color="green"
-                              class="ma-2 white--text"
-                              to="/schedule"
-                            >
-                              checkout
-                              <v-icon right dark>mdi-playlist-check</v-icon>
-                            </v-btn>
-                          </v-card-actions>
+          <v-card dense>
+            <v-card-subtitle v-if="addedToCart.length == 0">Cart is empty</v-card-subtitle>
+            <div v-else>
+              <v-card-actions>
+                <v-card-title>
+                  Total: ${{ addedToCart.length * 5 }}
+                </v-card-title>
+                <v-spacer />
+                <v-btn color="green" class="ma-2 white--text" to="/schedule">
+                  checkout
+                  <v-icon right dark>mdi-playlist-check</v-icon>
+                </v-btn>
+              </v-card-actions>
+              <v-list>
+                <div v-for="item in addedToCart" :key="item.id">
+                  <v-divider :inset="inset"></v-divider>
+                  <v-list-item>
+                    <v-list-item-content>
+                      <v-list-item-title v-text="item.name"></v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                </div>
+              </v-list>
+            </div>
           </v-card>
         </v-col>
       </v-row>
@@ -102,33 +88,40 @@ export default {
   data() {
     return {
       menu: "",
-      addedToCart: {hi:"hello"}
+      addedToCart: []
     };
   },
 
   methods: {
-    addToCart: function (item) {
-      if(!this.addedToCart[item.name]){
-        this.$set(this.addedToCart, item.name, 1);
-        
-      }
-      else{
-        let current = this.addedToCart[item.name];
-        this.$set(this.addedToCart, item.name, current + 1);
-        console.log(this.addedToCart);
-      }
+    addToCart: function(item) {
+      this.addedToCart.push(item);
+    },
+    removeFromCart: function(item) {
+      this.addedToCart.push(item);
+    },
+    getHello: function() {
+      return "hi";
+    },
+    getItemPrice: function() {
+      return "$5";
+      // axios
+      //   .get("/api/dominos/pricing/" + itemCode)
+      //   .then(response => {
+      //     console.log(response);
+      //     return response;
+      //   })
+      //   .catch(error => console.log(error));
     }
   },
 
   created: function() {
-  
-  axios
-    .get('/api/dominos/menu')
-    .then(response => {
-      console.log(response);
-      this.menu = response.data;
-    })
-    .catch(error => console.log(error));
+    axios
+      .get("/api/dominos/menu")
+      .then(response => {
+        console.log(response);
+        this.menu = response.data;
+      })
+      .catch(error => console.log(error));
   }
 };
 </script>
