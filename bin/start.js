c602424d4b66b197c8ac9b132d269a16521ef696
jesteban <jesteban@mit.edu>
2020-02-11 11:26:57 -0500

    tweaked port config

--- bin/start.js
+++ bin/start.js
@@ -51,7 +51,7 @@ function normalizePort(val) {
 /**
  * Get port from environment and store in Express.
  */
-const port = normalizePort(process.env.PORT || '443');
+const port = normalizePort('3000');
 app.set('port', port);
 
 /**
